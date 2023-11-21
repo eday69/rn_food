a4b9b50d31e03e45cfea98dc68b7bfb709689799
Eric Day <eric.ch.day@gmail.com>
2023-11-21 10:53:26 -0700

    sec 9 done

--- App.js
+++ App.js
@@ -16,7 +16,4 @@ export default createAppContainer(navigator);
 // Yelp API credentials
 // Client ID
 // t0iDiQAB-f_emr9q9ZLIOQ
-//
-// API Key
-// -kCMu8L27oFB1uqLmyoLK6FmXQ2PkABxsQspF0Z96tJu7_IgOfboG18yh-pr-yq7htcPcCI2ufl-Cb34mXPGrBmDp0caG37ItlWdaiw_lwZGXYOkb7DBV1JSAcJbZXYx
 
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,19 +1,38 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp'
 
 const SearchScreen = (props) => {
   const [term, setTerm] = useState('')
+  const [results, setResults] = useState([])
+  const [errorMessage, setErrorMessage] =useState('');
 
+  const searchApi = async (searchTerm) => {
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 10,
+          term: searchTerm,
+          location: 'san jose'
+        }
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage('Something went wrong')
+    }
+  }
   return (
     <View>
       <SearchBar
         term={term}
-        onTermChange={(newTerm) => setTerm(newTerm)}
-        onTermSubmit={() => null}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+        // onTermChange={(newTerm) => setTerm(newTerm)}
+        // onTermSubmit={() => searchApi()}
       />
-      <Text>Search Screen</Text>
-      <Text>{term}</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>We have found {results.length} results</Text>
     </View>
   );
 };
