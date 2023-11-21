6dbba4c79139f895478db0207d9d3674407c80d2
Eric Day <eric.ch.day@gmail.com>
2023-11-20 19:26:41 -0700

    sec 8 part I

---
+++ src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const SearchBar = () => {
+  return (
+    <View>
+      <Text>Search Bar Screen</Text>
+    </View>
+  );
+};
+
+const styles =  StyleSheet.create({});
+
+export default SearchBar;
