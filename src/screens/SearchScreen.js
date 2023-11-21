c76cc688fffbb30ec8a5a60f0cf477c5fc56e060
Eric Day <eric.ch.day@gmail.com>
2023-11-21 09:23:35 -0700

    sec 8 done

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import SearchBar from '../components/SearchBar';
 
 const SearchScreen = (props) => {
+  const [term, setTerm] = useState('')
+
   return (
     <View>
+      <SearchBar
+        term={term}
+        onTermChange={(newTerm) => setTerm(newTerm)}
+        onTermSubmit={() => null}
+      />
       <Text>Search Screen</Text>
+      <Text>{term}</Text>
     </View>
   );
 };
